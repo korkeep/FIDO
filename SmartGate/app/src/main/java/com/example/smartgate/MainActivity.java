8f2b0c668eb2793d4c0b73cd5bf008d6583e477a
KSS <korkeep@naver.com>
2020-03-20 17:24:11 +0900

    Firebase Push-Alarm Complete

---
+++ SmartGate/app/src/main/java/com/example/smartgate/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.smartgate;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import java.util.Objects;
+
+import static com.example.smartgate.FireBaseService.setCustomToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(!task.isSuccessful()){
+                    Log.w("SmartGate Log", "getInstanceID Failed", task.getException());
+                    return;
+                }
+                String token = Objects.requireNonNull(task.getResult()).getToken();
+                setCustomToast(MainActivity.this, token);
+            }
+        });
+    }
+}
